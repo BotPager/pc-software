2f7d4dec0ae5e6060e99327901a9226189d18430
dhwisema <dhwisema@ncsu.edu>
2026-02-05 00:52:34 -0500

    fix directory structure

--- software/App/teams.py
+++
@@ -1,7 +0,0 @@
-class Team:
-    def __init__(self, name: str, pid: str):
-        self.name = name            # team number (string or int)
-        self.pid = pid              # hex pager ID (as string)
-
-    def __repr__(self):
-        return f"Team(name={self.name}, pid={self.pid})"
